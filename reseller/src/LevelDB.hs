112a3843c4faf4b49a1327033ffa2e226da9617a
aravind mallapureddy <aravindgopal.ag@gmail.com>
2020-09-03 23:26:01 +0530

    added missing file

---
+++ reseller/src/LevelDB.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module LevelDB
+    ( getDBPath
+    , getValue
+    , putValue
+    , deleteValue
+    ) where
+
+import Control.Monad.IO.Unlift (MonadUnliftIO)
+import Control.Monad.Trans.Resource (runResourceT)
+import Data.ByteString (ByteString)
+import Data.Default (def)
+import Database.LevelDB (bloomFilter, createIfMissing, defaultOptions, delete, filterPolicy, get, open, put)
+
+-- | This is path for the Database location
+getDBPath :: String
+getDBPath = "/tmp/allpayproxy"
+
+-- | Returns Value from database corresponding to given key
+getValue :: (MonadUnliftIO m) => ByteString -> m (Maybe ByteString)
+getValue key =
+    runResourceT $ do
+        bloom <- bloomFilter 10
+        db <- open getDBPath defaultOptions {createIfMissing = True, filterPolicy = Just . Left $ bloom}
+        get db def key
+
+-- | Stores given (Key,Value) pair in database
+putValue :: (MonadUnliftIO m) => ByteString -> ByteString -> m ()
+putValue key value =
+    runResourceT $ do
+        bloom <- bloomFilter 10
+        db <- open getDBPath defaultOptions {createIfMissing = True, filterPolicy = Just . Left $ bloom}
+        put db def key value
+        return ()
+
+-- | Deletes Value from database corresponding to given key
+deleteValue :: (MonadUnliftIO m) => ByteString -> m ()
+deleteValue key =
+    runResourceT $ do
+        bloom <- bloomFilter 10
+        db <- open getDBPath defaultOptions {createIfMissing = True, filterPolicy = Just . Left $ bloom}
+        delete db def key
+        return ()
